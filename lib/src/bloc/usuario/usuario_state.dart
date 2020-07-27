8b93acd685ace5015fa05d853774d9c0ead331f4
Diego Armira <201220159@ingenieria.usac.edu.gt>
2020-07-26 21:18:00 -0600

    Revert "Feature/home page bloc diegoarmira"

--- lib/src/bloc/usuario/usuario_state.dart
+++
@@ -1,8 +0,0 @@
-class UsuarioState {
-  final int  sesion;
-
-  const UsuarioState({this.sesion});
-
-  factory UsuarioState.initial() => UsuarioState(sesion: 0);
-  factory UsuarioState.update(val) => UsuarioState (sesion: val);
-}
